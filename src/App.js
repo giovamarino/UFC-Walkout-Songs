0f683d37149d437c7329b179b57bbcd466adb671
Giovanni Marino <giovanni-marino@lambdastudents.com>
2022-05-02 20:15:35 -0400

    added Error page and dropdown menu works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { useNavigate, Route, Routes } from "react-router-dom";
 
 import DeivesonFigueiredo from "./fighter components/DeivesonFigueiredo";
 import AljamainSterling from "./fighter components/AljamainSterling";
@@ -18,74 +13,63 @@ import RoseNamajunas from "./fighter components/RoseNamajunas";
 import ValentinaShevchenko from "./fighter components/ValentinaShevchenko";
 import JuliannaPeña from "./fighter components/JuliannaPeña";
 import AmandaNunes from "./fighter components/AmandaNunes";
+import Error from "./Error";
 
 const App = () => {
   let [fighter, setFighter] = useState(`Deiveson Figueiredo`);
+  const navigate = useNavigate();
 
   return (
-    <Router>
-      <div className="container">
-        <img src="/UFC_logo.png" alt="UFC Logo" className="UFCLogo"></img>
-        <h1>{fighter}</h1>
-        <Routes>
-          <Route path="/" element={<DeivesonFigueiredo />} />
-          <Route path="/DeivesonFigueiredo" element={<DeivesonFigueiredo />} />
-          <Route path="/AljamainSterling" element={<AljamainSterling />} />
-          <Route
-            path="/AlexanderVolkanovski"
-            element={<AlexanderVolkanovski />}
-          />
-          <Route path="/CharlesOliveira" element={<CharlesOliveira />} />
-          <Route path="/KamaruUsman" element={<KamaruUsman />} />
-          <Route path="/IsraelAdesanya" element={<IsraelAdesanya />} />
-          <Route path="/GloverTeixeira" element={<GloverTeixeira />} />
-          <Route path="/FrancisNgannou" element={<FrancisNgannou />} />
-          <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
-          <Route
-            path="/ValentinaShevchenko"
-            element={<ValentinaShevchenko />}
-          />
-          <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
-          <Route path="/JuliannaPena" element={<JuliannaPeña />} />
-          <Route path="/AmandaNunes" element={<AmandaNunes />} />
-        </Routes>
-        <div className="inputChild">
-          <label htmlFor="fighterLabel">Fighter: </label>
-          <select
-            id="fighterLabel"
-            onChange={(e) => {
-              setFighter(e.target.value);
-            }}
-          >
-            <option value="DeivesonFigueiredo">Deiveson Figueiredo</option>
-            <option value="AljamainSterling">Aljamain Sterling</option>
-            <option value="AlexanderVolkanovski">Alexander Volkanovski</option>
-            <option value="CharlesOliveira">Charles Oliveira</option>
-            <option value="KamaruUsman">Kamaru Usman</option>
-            <option value="IsraelAdesanya">Israel Adesanya</option>
-            <option value="GloverTeixeira">Glover Teixeira</option>
-            <option value="FrancisNgannou">Francis Ngannou</option>
-            <option value="RoseNamajunas">Rose Namajunas</option>
-            <option value="ValentinaShevchenko">Valentina Shevchenko</option>
-            <option value="JuliannaPeña">Julianna Peña</option>
-            <option value="AmandaNunes">Amanda Nunes</option>
-
-            {/* redirect to deiveson? use the Navigate component */}
-          </select>
-          <button
-            onClick={() => {
-              // useNavigate(`/AljamainSterling`);
-              const select = document.getElementById("fighterLabel");
-              const value = select.options[select.selectedIndex].value;
-              console.log(value);
-              // useNavigate()(`/AljamainSterling`);
-            }}
-          >
-            select fighter
-          </button>
-        </div>
+    <div className="container">
+      <img src="/UFC_logo.png" alt="UFC Logo" className="UFCLogo"></img>
+      <h1>{fighter}</h1>
+      <Routes>
+        <Route path="/" element={<DeivesonFigueiredo />} />
+        <Route path="/DeivesonFigueiredo" element={<DeivesonFigueiredo />} />
+        <Route path="/AljamainSterling" element={<AljamainSterling />} />
+        <Route
+          path="/AlexanderVolkanovski"
+          element={<AlexanderVolkanovski />}
+        />
+        <Route path="/CharlesOliveira" element={<CharlesOliveira />} />
+        <Route path="/KamaruUsman" element={<KamaruUsman />} />
+        <Route path="/IsraelAdesanya" element={<IsraelAdesanya />} />
+        <Route path="/GloverTeixeira" element={<GloverTeixeira />} />
+        <Route path="/FrancisNgannou" element={<FrancisNgannou />} />
+        <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
+        <Route path="/ValentinaShevchenko" element={<ValentinaShevchenko />} />
+        <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
+        <Route path="/JuliannaPena" element={<JuliannaPeña />} />
+        <Route path="/AmandaNunes" element={<AmandaNunes />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <div className="inputChild">
+        <label htmlFor="fighterLabel">Fighter: </label>
+        <select
+          id="fighterLabel"
+          onChange={(e) => {
+            setFighter(e.target.value);
+            navigate(`/${e.target.value}`);
+            console.log(e);
+            console.log(e.target.value);
+            console.log(e.nativeEvent.target.innerText[e.target.value]);
+          }}
+        >
+          <option value="DeivesonFigueiredo">Deiveson Figueiredo</option>
+          <option value="AljamainSterling">Aljamain Sterling</option>
+          <option value="AlexanderVolkanovski">Alexander Volkanovski</option>
+          <option value="CharlesOliveira">Charles Oliveira</option>
+          <option value="KamaruUsman">Kamaru Usman</option>
+          <option value="IsraelAdesanya">Israel Adesanya</option>
+          <option value="GloverTeixeira">Glover Teixeira</option>
+          <option value="FrancisNgannou">Francis Ngannou</option>
+          <option value="RoseNamajunas">Rose Namajunas</option>
+          <option value="ValentinaShevchenko">Valentina Shevchenko</option>
+          <option value="JuliannaPena">Julianna Peña</option>
+          <option value="AmandaNunes">Amanda Nunes</option>
+        </select>
       </div>
-    </Router>
+    </div>
   );
 };
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,10 @@
+const Error = () => {
+  return (
+    <div className="Error">
+      We could not find the page that you requested. Please select another
+      fighter from the dropdown menu.
+    </div>
+  );
+};
+
+export default Error;
