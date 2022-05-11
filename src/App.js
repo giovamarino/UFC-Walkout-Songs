b75920125995a7be1c90d895902b7dc67210588e
Giovanni Marino <giovanni-marino@lambdastudents.com>
2022-05-10 22:28:29 -0400

    added semantic tags

--- src/App.js
+++ src/App.js
@@ -21,54 +21,61 @@ const App = () => {
 
   return (
     <div className="container">
-      <img src="/UFC_logo.png" alt="UFC Logo" className="UFCLogo"></img>
-      <h1>{fighter}</h1>
-      <Routes>
-        <Route path="/" element={<DeivesonFigueiredo />} />
-        <Route path="/DeivesonFigueiredo" element={<DeivesonFigueiredo />} />
-        <Route path="/AljamainSterling" element={<AljamainSterling />} />
-        <Route
-          path="/AlexanderVolkanovski"
-          element={<AlexanderVolkanovski />}
-        />
-        <Route path="/CharlesOliveira" element={<CharlesOliveira />} />
-        <Route path="/KamaruUsman" element={<KamaruUsman />} />
-        <Route path="/IsraelAdesanya" element={<IsraelAdesanya />} />
-        <Route path="/GloverTeixeira" element={<GloverTeixeira />} />
-        <Route path="/FrancisNgannou" element={<FrancisNgannou />} />
-        <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
-        <Route path="/ValentinaShevchenko" element={<ValentinaShevchenko />} />
-        <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
-        <Route path="/JuliannaPena" element={<JuliannaPeña />} />
-        <Route path="/AmandaNunes" element={<AmandaNunes />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <div className="inputChild">
-        <label htmlFor="fighterLabel">Fighter: </label>
-        <select
-          id="fighterLabel"
-          onChange={(e) => {
-            setFighter(e.target.value);
-            navigate(`/${e.target.value}`);
-            console.log(e);
-            console.log(e.target.value);
-            console.log(e.nativeEvent.target.innerText[e.target.value]);
-          }}
-        >
-          <option value="DeivesonFigueiredo">Deiveson Figueiredo</option>
-          <option value="AljamainSterling">Aljamain Sterling</option>
-          <option value="AlexanderVolkanovski">Alexander Volkanovski</option>
-          <option value="CharlesOliveira">Charles Oliveira</option>
-          <option value="KamaruUsman">Kamaru Usman</option>
-          <option value="IsraelAdesanya">Israel Adesanya</option>
-          <option value="GloverTeixeira">Glover Teixeira</option>
-          <option value="FrancisNgannou">Francis Ngannou</option>
-          <option value="RoseNamajunas">Rose Namajunas</option>
-          <option value="ValentinaShevchenko">Valentina Shevchenko</option>
-          <option value="JuliannaPena">Julianna Peña</option>
-          <option value="AmandaNunes">Amanda Nunes</option>
-        </select>
-      </div>
+      <header>
+        <img src="/UFC_logo.png" alt="UFC Logo" className="UFCLogo"></img>
+      </header>
+      <main>
+        <h1>{fighter}</h1>
+        <Routes>
+          <Route path="/" element={<DeivesonFigueiredo />} />
+          <Route path="/DeivesonFigueiredo" element={<DeivesonFigueiredo />} />
+          <Route path="/AljamainSterling" element={<AljamainSterling />} />
+          <Route
+            path="/AlexanderVolkanovski"
+            element={<AlexanderVolkanovski />}
+          />
+          <Route path="/CharlesOliveira" element={<CharlesOliveira />} />
+          <Route path="/KamaruUsman" element={<KamaruUsman />} />
+          <Route path="/IsraelAdesanya" element={<IsraelAdesanya />} />
+          <Route path="/GloverTeixeira" element={<GloverTeixeira />} />
+          <Route path="/FrancisNgannou" element={<FrancisNgannou />} />
+          <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
+          <Route
+            path="/ValentinaShevchenko"
+            element={<ValentinaShevchenko />}
+          />
+          <Route path="/RoseNamajunas" element={<RoseNamajunas />} />
+          <Route path="/JuliannaPena" element={<JuliannaPeña />} />
+          <Route path="/AmandaNunes" element={<AmandaNunes />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <div className="inputChild">
+          <label htmlFor="fighterLabel">Fighter: </label>
+          <select
+            id="fighterLabel"
+            onChange={(e) => {
+              setFighter(e.target.value);
+              navigate(`/${e.target.value}`);
+              console.log(e);
+              console.log(e.target.value);
+              console.log(e.nativeEvent.target.innerText[e.target.value]);
+            }}
+          >
+            <option value="DeivesonFigueiredo">Deiveson Figueiredo</option>
+            <option value="AljamainSterling">Aljamain Sterling</option>
+            <option value="AlexanderVolkanovski">Alexander Volkanovski</option>
+            <option value="CharlesOliveira">Charles Oliveira</option>
+            <option value="KamaruUsman">Kamaru Usman</option>
+            <option value="IsraelAdesanya">Israel Adesanya</option>
+            <option value="GloverTeixeira">Glover Teixeira</option>
+            <option value="FrancisNgannou">Francis Ngannou</option>
+            <option value="RoseNamajunas">Rose Namajunas</option>
+            <option value="ValentinaShevchenko">Valentina Shevchenko</option>
+            <option value="JuliannaPena">Julianna Peña</option>
+            <option value="AmandaNunes">Amanda Nunes</option>
+          </select>
+        </div>
+      </main>
     </div>
   );
 };
